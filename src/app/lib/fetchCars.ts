0a5236f2f2999e0638405697b3043b0c21166a70
nnadya <n.d.niki2012@gmail.com>
2025-06-18 21:50:52 +0300

    update fetchCars.ts

--- src/app/lib/fetchCars.ts
+++ src/app/lib/fetchCars.ts
@@ -4,15 +4,34 @@ import { CarsResponse } from "../types/carsResponse";
 
 //const url = 'https://plex-parser.ru-rating.ru/cars';
 
-const fetchCars = async (page = 1): Promise<CarsResponse> => {
-  const limit = 12;
-  const response = await fetch(`https://plex-parser.ru-rating.ru/cars?_limit=${limit}&_page=${page}`);
+const fetchCars = async (page = 1, sort?: string, order?: string): Promise<CarsResponse> => {
+  const params = new URLSearchParams({
+    _limit: "12",
+    _page: page.toString(),
+  });
+
+  if (sort && order) {
+    params.set("_sort", sort);
+    params.set("_order", order);
+  }
+
+  const response = await fetch(`https://plex-parser.ru-rating.ru/cars?${params.toString()}`);
   if (!response.ok) throw new Error("Failed to fetch cars");
-  console.log(response);
+  console.log('response ->', response);
   return response.json();
 };
 
 export default fetchCars;
+
+/* 
+const fetchCars = async (page = 1): Promise<CarsResponse> => {
+  const limit = 12;
+  const response = await fetch(`https://plex-parser.ru-rating.ru/cars?_limit=${limit}&_page=${page}`);
+  if (!response.ok) throw new Error("Failed to fetch cars");
+  console.log('response ->', response);
+  return response.json();
+};
+*/
 
 /*
   return {
