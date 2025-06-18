ce5a09979db8ff887f26260052d9aa1d46f52a85
nnadya <n.d.niki2012@gmail.com>
2025-06-18 21:49:54 +0300

    update createPageLink

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -4,10 +4,21 @@ import getPageNumbers from "../utils/getPageNumbers";
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
+  sort?: string;
+  order?: string;
 }
 
-const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
-  const createPageLink = (page: number) => `/?page=${page}`;
+const Pagination = ({ currentPage, totalPages, sort, order }: PaginationProps) => {
+  //const createPageLink = (page: number) => `/?page=${page}`;
+  const createPageLink = (page: number) => {
+    const params = new URLSearchParams();
+    params.set("page", page.toString());
+
+    if (sort) params.set("_sort", sort);
+    if (order) params.set("_order", order);
+
+    return `/?${params.toString()}`;
+  };
 
   const hasPrev = currentPage > 1;
   const hasNext = currentPage < totalPages;
