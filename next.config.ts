ea64c947d7ab2fb5338341d7543779c8e2f8cb8d
nnadya <n.d.niki2012@gmail.com>
2025-06-20 17:41:30 +0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ru-msk-dr3-1.store.cloud.mts.ru'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ru-msk-dr3-1.store.cloud.mts.ru',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
